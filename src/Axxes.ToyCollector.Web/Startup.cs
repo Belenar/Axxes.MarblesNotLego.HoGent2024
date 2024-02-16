5b19fcf5c6a02bf3fb939cfc4c975405c4eb90a9
Hannes Lowette <lowette.hannes@gmail.com>
2024-02-16 11:22:41 +0100

    Serializer

--- src/Axxes.ToyCollector.Web/Startup.cs
+++ src/Axxes.ToyCollector.Web/Startup.cs
@@ -47,8 +47,8 @@ namespace Axxes.ToyCollector.Web
             // Allows the passing of JSON $type parameters (required for inherited types)
             mvcBuilder.AddNewtonsoftJson(jsonOptions =>
             {
-                // TODO 3: Add inherited types registrar
-                
+                jsonOptions.SerializerSettings.Converters.Add(
+                    new InheritedTypesJsonConverter(inheritedTypesRegistry));
             });
 
             services.Configure<DatabaseConnectionStrings>(Configuration.GetSection("ConnectionStrings"));
